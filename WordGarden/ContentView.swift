d01cbe12a3a31f6730305e5304d833ca919f1da9
Richard Gagg <r.gagg@outlook.com>
2025-01-23 10:03:10 +1100

    Initial Commit

---
+++ WordGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGarden
+//
+//  Created by Richard Gagg on 23/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Richard Gagg on 23/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
